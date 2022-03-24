d300c0db7e640a126eec61df6159c54fe26adaf6
Radhamohan Upadhyay <radhamohan.upadhyaya@gmail.com>
2022-03-24 15:21:32 +0530

    Upload Work Done

--- Controllers/CommonController.cs
+++ Controllers/CommonController.cs
@@ -1,8 +1,14 @@
-﻿using ES_HomeCare_API.Model;
+﻿using ES_HomeCare_API.Helper;
+using ES_HomeCare_API.Model;
 using ES_HomeCare_API.WebAPI.Service.IService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.IO;
+
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using WebAPI_SAMPLE.Model;
 
@@ -31,7 +37,43 @@ namespace ES_HomeCare_API.Controllers
             return Ok(await comSrv.GetMasterList(typeId));
         }
 
+        
 
+
+        [HttpPost("UploadFile"), DisableRequestSizeLimit]        
+        [ProducesResponseType(typeof(ServiceResponse<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<string>), StatusCodes.Status400BadRequest)]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var files = Request.Form.Files;
+              
+               
+
+                if (files.Any(f => f.Length == 0))
+                {
+                    return BadRequest();
+                }
+
+                foreach (var file in files)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                   //you can add this path to a list and then return all dbPaths to the client if require
+
+                    Stream fs = file.OpenReadStream();
+                    AmazonUploader uploader = new AmazonUploader();
+                    uploader.sendMyFileToS3(fs, "eshomecarewebapp", fileName);
+                }
+
+                return Ok("All the files are successfully uploaded.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+           
+        }
 
 
 
---
+++ Helper/AmazonUploader.cs
@@ -0,0 +1,64 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ES_HomeCare_API.Helper
+{
+    public class AmazonUploader
+    {
+        public void sendMyFileToS3(System.IO.Stream localFilePath, string bucketName, string fileNameInS3)
+        {
+
+            try
+            {
+
+                IAmazonS3 client = new AmazonS3Client("AKIAVSTZJGNSR2UPYM6F", "d6jG9K7ZOIyycQzNX31V08EzXn8P4AvFIyx5AsWa", RegionEndpoint.USEast1);
+
+                RunFolderCreationDemo(client, "Navneet");
+                TransferUtility utility = new TransferUtility(client);
+                TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
+                request.BucketName = bucketName;
+
+                request.Key = fileNameInS3;
+                request.InputStream = localFilePath;
+                request.CannedACL = S3CannedACL.PublicReadWrite;
+                utility.Upload(request);
+
+                //bool status = CheckFile(client, fileNameInS3, sourceFilePath);
+                //return status;
+            }
+            catch (System.Exception ex)
+            {
+
+                // return false;
+            }
+
+
+        }
+
+
+
+        public void RunFolderCreationDemo(IAmazonS3 s3Client, string FolderName)
+        {
+            try
+            {
+
+                PutObjectRequest folderRequest = new PutObjectRequest();
+                String delimiter = "/";
+                folderRequest.BucketName = "eshomecarewebapp";
+                String folderKey = string.Concat(FolderName, delimiter);
+                folderRequest.Key = folderKey;
+                folderRequest.InputStream = new MemoryStream(new byte[0]);
+                Task<PutObjectResponse> folderResponse = s3Client.PutObjectAsync(folderRequest);
+            }
+            catch (AmazonS3Exception e)
+            {
+
+            }
+        }
+    }
+}
