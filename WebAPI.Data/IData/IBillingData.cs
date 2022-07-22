f17b94063f331fa9f409d60473a842a11ae160f2
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2022-07-22 12:40:25 +0530

    update

--- WebAPI.Data/IData/IBillingData.cs
+++ WebAPI.Data/IData/IBillingData.cs
@@ -13,13 +13,10 @@ namespace ES_HomeCare_API.WebAPI.Data.IData
         Task<ServiceResponse<IEnumerable<PayerModel>>> GetPayerList();
         Task<ServiceResponse<string>> DelPayer(int PayerId);
         Task<ServiceResponse<BillingSummaryInfoModel>> GetBillingSummaryInfo(int userId);
-
         Task<ServiceResponse<IEnumerable<BillingStatusViewModel>>> GetBillingStatusList();
         Task<ServiceResponse<IEnumerable<PayrollStatusViewModel>>> GetPayrollStatusList();
-
         Task<ServiceResponse<IEnumerable<ClientSchedule>>> GetScheduleBilling();
-        Task<ServiceResponse<IEnumerable<ClientSchedule>>> GetScheduleBilling(SearchSchedule model);
-        Task<ServiceResponse<IEnumerable<ScheduleBillingModel>>> GetScheduleBilling(SearchSchedule model);
+        Task<ServiceResponse<IEnumerable<ClientSchedule>>> GetScheduleBilling(SearchSchedule model);      
         Task<ServiceResponse<BillingPayerRateViewModel>> GetBillingPayerRate(long payerId, long clientId);
     }
 }
